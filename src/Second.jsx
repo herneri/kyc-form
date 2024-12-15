92446084ea7f5dfdecbea6061dffa0696d3f0b57
Eric Hernandez <herneri@protonmail.com>
2024-12-14 20:24:00 -0600

    docs: Prepend license notice to two component files

--- src/Second.jsx
+++ src/Second.jsx
@@ -1,3 +1,22 @@
+/*
+    Copyright (C) 2024  Eric Hernandez
+
+    This file is part of kyc-form.
+
+    kyc-form is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    kyc-form is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with kyc-form.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { useNavigate } from 'react-router-dom';
 
 function SecondForm() {
